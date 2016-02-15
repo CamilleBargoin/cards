9f3986a60702e9a9c6c02416376d9ee0dbad474d
unknown <cbargoin@gmail.com>
2016-02-15 14:46:15 +0100

    adding random background on landing page

--- Cards/public/javascripts/index.js
+++ Cards/public/javascripts/index.js
@@ -10,4 +10,11 @@ Zepto(function($){
         $("#registerForm").hide();
     });
 
+
+    // ARTWORK BY CONCEPT-CUBE
+    var backgrounds =[ '/images/backgrounds/background2.jpg',  '/images/backgrounds/background3.jpg', '/images/backgrounds/background4.jpg'];
+
+    $("#index").css({
+        background: "url(" + backgrounds[Math.floor(Math.random() * 3)] + ")"
+    });
 });
